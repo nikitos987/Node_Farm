6d6cecce9d38d02c54c8456c3a1818199d9a89f7
Nikita <js.python.st@gmail.com>
2021-05-16 16:05:45 +0100

    commit after lesson 11 - Creating the server

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
+const http = require('http');
 
+/////////////////////////////////////////////////////////////
+// FILES
 /*
 // Blocking, synchronous way
 const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
@@ -9,7 +12,7 @@ const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on 
 fs.writeFileSync('./txt/output.txt', textOut);
 console.log('File has been written');
 */
-
+/*
 // Non-blocking, asynchronous way
 fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
   if (err) return console.log('ERROR!!!');
@@ -27,3 +30,15 @@ fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
 console.log('Will read file');
 console.log('Will read file');
 console.log('Will read file');
+*/
+
+///////////////////////////////////////////////////////////////
+// SERVER
+const server = http.createServer((req, res) => {
+  //   console.log(req);
+  res.end('Hello from the server!');
+});
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to request on port 8000');
+});
