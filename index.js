6d58af5d2360d7ada1940f5a1ca1493a74fd10fa
Nikita <js.python.st@gmail.com>
2021-05-16 21:01:59 +0100

    commit after lesson 13 - Building simple API

--- index.js
+++ index.js
@@ -35,6 +35,9 @@ console.log('Will read file');
 
 ///////////////////////////////////////////////////////////////
 // SERVER
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
   //   console.log(req.url);
 
@@ -44,6 +47,11 @@ const server = http.createServer((req, res) => {
     res.end('This is the OVERVIEW');
   } else if (pathName === '/product') {
     res.end('This is a PRODUCT');
+  } else if (pathName === '/api') {
+    res.writeHead(200, {
+      'Content-type': 'application/json',
+    });
+    res.end(data);
   } else {
     res.writeHead(404, {
       'Content-type': 'text/html',
