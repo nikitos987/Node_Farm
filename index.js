ee4bedf4f53a204e90b11727e1124579dcd6ef9d
Nikita <js.python.st@gmail.com>
2021-05-17 12:58:15 +0100

    commit after lesson 16 - Project basically finished

--- index.js
+++ index.js
@@ -67,11 +67,11 @@ const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
   //   console.log(req.url);
-
-  const pathName = req.url;
+  //   console.log(url.parse(req.url, true));
+  const { query, pathname } = url.parse(req.url, true);
 
   //   Overview page
-  if (pathName === '/' || pathName === '/overview') {
+  if (pathname === '/' || pathname === '/overview') {
     res.writeHead(200, { 'Content-type': 'text/html' });
 
     const cardsHtml = dataObj
@@ -83,11 +83,15 @@ const server = http.createServer((req, res) => {
     res.end(output);
 
     // Produc page
-  } else if (pathName === '/product') {
-    res.end('This is a PRODUCT');
+  } else if (pathname === '/product') {
+    // console.log(query);
+    res.writeHead(200, { 'Content-type': 'text/html' });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
 
     // API
-  } else if (pathName === '/api') {
+  } else if (pathname === '/api') {
     res.writeHead(200, {
       'Content-type': 'application/json',
     });
